c32bce120ff5d3f85d429468651c9b857781621f
Daniel Chipolesk <168142021+Chipolesk@users.noreply.github.com>
2024-10-05 11:39:10 -0300

    Update login.php

--- backend/login.php
+++ backend/login.php
@@ -1,8 +1,10 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://chipolesk.github.io/FECIP/index.html");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
