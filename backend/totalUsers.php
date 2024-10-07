17858922a32396c04915050e8d3099dcab81f07c
Daniel Chipolesk <168142021+Chipolesk@users.noreply.github.com>
2024-10-07 17:01:04 -0300

    Add files via upload

---
+++ backend/totalUsers.php
@@ -0,0 +1,55 @@
+<?php
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+header('Content-Type: application/json'); 
+
+try {
+    // Informações de conexão
+    $serverName = "digitalcoreserver.database.windows.net";
+    $connectionOptions = array(
+        "Database" => "DigitalCoreDB",
+        "Uid" => "DIGITAL.CORE",
+        "PWD" => "@FECIP2K24",
+        "Encrypt" => true,
+        "TrustServerCertificate" => false,
+        "LoginTimeout" => 30,
+    );
+
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    
+    if ($conn === false) {
+        throw new Exception('Falha na conexão com o SQL Server: ' . json_encode(sqlsrv_errors()));
+    }
+
+    // Query para obter o nome do jogo e os acessos
+    $sql = "SELECT nome_jogo, acessos_jogo FROM jogos";
+
+    $stmt = sqlsrv_query($conn, $sql);
+
+    if ($stmt === false) {
+        throw new Exception('Falha ao consultar dados: ' . json_encode(sqlsrv_errors()));
+    }
+
+    $resultados = [];
+    
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        $resultados[$row['nome_jogo']] = $row['acessos_jogo'];
+    }
+
+    echo json_encode($resultados); // Retorna os resultados em formato JSON
+} catch (Exception $e) {
+    $response = array('status' => 'erro', 'message' => $e->getMessage());
+    echo json_encode($response);
+} finally {
+    if (isset($conn) && $conn !== false) {
+        sqlsrv_close($conn);
+    }
+}
+?>
