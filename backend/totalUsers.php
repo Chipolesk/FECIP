b4f1e65149669b1e1cd13d374e5dcd5c4d80d49d
Daniel Chipolesk <168142021+Chipolesk@users.noreply.github.com>
2024-10-07 17:54:13 -0300

    Update totalUsers.php

--- backend/totalUsers.php
+++ backend/totalUsers.php
@@ -1,3 +1,13 @@
+<?php
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 header('Content-Type: application/json'); 
 
 try {
@@ -47,3 +57,4 @@ try {
         sqlsrv_close($conn);
     }
 }
+?>
