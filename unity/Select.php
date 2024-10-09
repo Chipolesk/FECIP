fcc1794c5c702de5921033c993c44f2a764dee0c
Daviapenas <106933113+Daviapenas@users.noreply.github.com>
2024-10-09 00:54:26 -0300

    Create Select.php

---
+++ unity/Select.php
@@ -0,0 +1,40 @@
+<?php
+// Defina as informações de conexão
+$serverName = "digitalcoreserver.database.windows.net";
+$connectionOptions = array(
+    "Database" => "DigitalCoreDB",
+    "Uid" => "DIGITAL.CORE",
+    "PWD" => "@FECIP2K24",
+    "Encrypt" => true,
+    "TrustServerCertificate" => false,
+    "LoginTimeout" => 30,
+);
+
+// Estabelecer a conexão com o SQL Server
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if ($conn === false) {
+    die(json_encode(array('status' => 'erro', 'message' => 'Falha na conexão com o SQL Server: ' . print_r(sqlsrv_errors(), true))));
+}
+
+$colunas = $_POST['colunas'];
+$tabela = $_POST['tabela'];
+
+// Buscar dados na tabela especificada
+$sql = "SELECT $colunas FROM $tabela";
+$stmt = sqlsrv_query($conn, $sql);
+
+if ($stmt === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
+$resultados = array();
+while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+    $resultados[] = $row;
+}
+
+echo json_encode($resultados);
+
+// Fechar a conexão
+sqlsrv_close($conn);
+?>
